0e3dcea3ec16a798569bc804eb299b1eaa6e3171
elementxenon <56176691+elementxenon@users.noreply.github.com>
2023-04-26 15:05:28 -0400

    fixed LCD code

--- firmware/tempsensor.X/tempsensor.c
+++ firmware/tempsensor.X/tempsensor.c
@@ -15,7 +15,7 @@
 /*Preprocessor Macros*/
 
 #define _XTAL_FREQ 1000000     //Set XC8 oscillator freq
-
+#define I2C_SLAVE_ADDR 0x4D
 
 /*Configuration Bits*/
 
@@ -26,24 +26,110 @@
 
 
 /*Function Prototypes*/
-
+void updateLCD(int);
+int queryTemp();
+void writeStaticLCD();
+void clear_sspif();
 
 /*Global Variables*/
-
+int tempdata;
+int update;
 
 
 void main(void) {
+    //Setup I/O
+    TRISA = 0x00;
+    TRISB = 0x00;
+    TRISC = 0x00;
+    TRISD = 0x00;
+    TRISBbits.RB0 = 1;
+    
+     //I2C Initialization
+    SSPADD = 0x05;              //SSPADD value for I2C clk
+    SSPCON1bits.SSPM = 1000;    //
+    
+    //Interrupt Initialization
+    INTCONbits.RBIF = 0;        //Reset PortB Interrupt flag
+    RCONbits.IPEN = 1;          //enable interrupt priority
+    INTCON2bits.RBIP = 1;       //High priority
+    INTCONbits.GIEH = 1;        //enable all high interrupts
+    
+   
+
+    //Initialization for LCD
     LCD_init();
-    TRISC = 0xF0;
-    TRISB = 0xFF;
     LCD_clear();
-    LCD_cmd(0x80);
-    LCD_txt("Temp:");
-    LCD_cmd(0xC2);
-    LCD_txt(0xF8);
-    LCD_txt("F");
+    writeStaticLCD();
+    
+    tempdata = 123;
+    update = 1;
+    
     while(1)
     {
-        
+        if(update == 1)
+        {
+            updateLCD(tempdata);
+            update = 0;
+        }
+    }
+     
+}
+void updateLCD(int temp)
+{
+    char td3 =  (temp % 10) + '0';
+    char td2 =  (temp % 100)/10 +'0';
+    char td1 =  (temp % 1000)/100 +'0';
+    LCD_cmd(0xC0);
+    LCD_data(td1);
+    LCD_data(td2);
+    LCD_data(td3);
+}
+void writeStaticLCD()
+{
+    LCD_cmd(0x80);
+    LCD_txt("Temp:");
+    LCD_cmd(0xC3);
+    LCD_txt("F");
+}
+int queryTemp()
+{
+    int rawtemp;
+    SSPCON2bits.SEN = 1;        //start condition enable
+    clear_sspif();
+    SSPBUF = I2C_SLAVE_ADDR ;
+    clear_sspif();
+    while(SSPCON2bits.ACKSTAT = 0){}
+    SSPBUF = 0x00;                  //read temp command
+    clear_sspif();
+    while(SSPCON2bits.ACKSTAT = 0){}
+    SSPCON2bits.SEN = 1;        //start condition enable
+    clear_sspif();
+    SSPBUF = I2C_SLAVE_ADDR ;
+    clear_sspif();
+    while(SSPCON2bits.ACKSTAT = 0){}
+    SSPCON2bits.RCEN = 1;           //RCEN
+    clear_sspif();
+    rawtemp = SSPBUF;
+    SSPCON2bits.ACKDT = 1;      //NACK
+    SSPCON2bits.PEN = 1;            //stop condition enable
+    clear_sspif();
+    return rawtemp;
+}
+void clear_sspif()
+{
+    while(PIR1bits.SSPIF = 0){}
+    PIR1bits.SSPIF = 0;
+}
+
+void __interrupt(high_priority) high_ISR(void)
+{
+    if(INTCONbits.RBIF == 1)
+    {
+        __delay_ms(10);
+        if(INTCONbits.RBIF == 1)
+        {
+        tempdata = 456;
+        update  = 1;
+        }  
     }
 }
